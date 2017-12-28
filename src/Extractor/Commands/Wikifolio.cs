3dff9b5b2cc58c66709c7607dc9155dbe107edc6
dschruf <david.schruf@wikifolio.com>
2017-12-28 17:23:21 +0100

    converted to command line app

---
+++ src/Extractor/Commands/Wikifolio.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+
+using McMaster.Extensions.CommandLineUtils;
+
+namespace Extractor.Commands
+{
+    [Command(
+         Name = "wikifolio",
+         Description = @"extracts a given wikifolio from production database in form of INSERT statements.",
+         ExtendedHelpText = @"
+Example Usage: 
+wikifolio 9B7F994D-3C9A-45B9-BB15-001A7EF522AA A61696C7-3343-4761-8EA7-1EB39A27E431 --withFees --withWikifolioTickData --outputfile ""gaxi.txt""
+wikifolio 9B7F994D-3C9A-45B9-BB15-001A7EF522AA A61696C7-3343-4761-8EA7-1EB39A27E431
+",
+         ShowInHelpText = true), HelpOption]
+    public class Wikifolio
+    {
+        private const string WikifolioGuidArgumentIdentifier = "WikifolioGuid";
+        private const string WikifolioOwnerGuidArgumentIdentifier = "WikifolioOwnerGuid";
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "You must specify the wikifolio guid")]
+        [Argument(
+            order: 0,
+            name: WikifolioGuidArgumentIdentifier,
+            description: "The guid of the wikifolio you want to extract - MUST BE SPECIFIED")]
+        public string WikifolioGuidArgument { get; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "You must specify an existing user guid for " + WikifolioOwnerGuidArgumentIdentifier)]
+        [Argument(
+            order: 1,
+            name: WikifolioOwnerGuidArgumentIdentifier,
+            description: "The guid of the user that will be the owner of the wikifolio - MUST BE SPECIFIED")]
+        public string WikifolioOwnerGuidArgument { get; }
+
+        [Option(
+            template: "--withWikifolioTickData",
+            optionType: CommandOptionType.NoValue,
+            Description = "If given, all wikifolio tick data will also be extracted")]
+        public bool WithWikifolioTickData { get; }
+
+        [Option(
+            template: "--withFees",
+            optionType: CommandOptionType.NoValue,
+            Description = "If given, all wikifolio fees will also be extracted")]
+        public bool WithFees { get; }
+
+        [Option(
+            template: "--outputfile",
+            optionType: CommandOptionType.SingleValue,
+            Description = "If given, the output will be written into the given file " + 
+                          "(its content will be overwritten, if it exists already)")]
+        public string OutputFile { get; }
+
+        private void OnExecute(CommandLineApplication app, IConsole console)
+        {
+            Guid wikifolioGuid;
+            Guid wikifolioOwnerGuid;
+            if (!this.TryParseGuid(this.WikifolioGuidArgument, out wikifolioGuid, WikifolioGuidArgumentIdentifier)
+                || !this.TryParseGuid(this.WikifolioOwnerGuidArgument, out wikifolioOwnerGuid, WikifolioOwnerGuidArgumentIdentifier))
+            {
+                app.ShowHelp();
+                return;
+            }
+
+            var wikifolioExtractor = new WikifolioExtractor(
+                wikifolioGuid,
+                wikifolioOwnerGuid,
+                this.WithWikifolioTickData,
+                this.WithFees);
+
+            TextWriter writer;
+            if (!this.TryParseOutputFile(out writer))
+            {
+                writer = console.Out;
+            }
+            wikifolioExtractor.WriteInserts(writer);
+        }
+
+        private bool TryParseGuid(string rawValue, out Guid outputGuid, string argumentName)
+        {
+            if (!Guid.TryParse(rawValue, out outputGuid))
+            {
+                Console.WriteLine(
+                    $"Could not parse argument: '{argumentName}'. It was: '{rawValue}', but should be a valid Guid");
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool TryParseOutputFile(out TextWriter writer)
+        {
+            writer = null;
+            if (string.IsNullOrEmpty(this.OutputFile))
+            {
+                return false;
+            }
+
+            try
+            {
+                writer = File.CreateText(this.OutputFile);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"error wrinter to output file: '{this.OutputFile}'");
+                Console.WriteLine(e);
+                return false;
+            }
+        }
+    }
+}
--- src/Extractor/Program.cs
+++ src/Extractor/Program.cs
@@ -1,16 +1,11 @@
-﻿using System;
+﻿using McMaster.Extensions.CommandLineUtils;
 
 namespace Extractor
 {
+    [HelpOption,
+    Subcommand("wikifolio", typeof(Commands.Wikifolio))]
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            var wikifolioGuid = new Guid("01DB9DB8-DC41-4FEF-8172-001F101573F9");
-            wikifolioGuid = new Guid("FF48D396-7D1D-4E3C-A9D2-0414173BC23F");
-            wikifolioGuid = new Guid("2F5842FC-977C-4E62-9028-2D3A8D00F9C1");
-            var userGuid = new Guid("B6AB34AA-9587-4F1E-A54C-2DCF23764FCE");
-            new WikifolioExtractor(wikifolioGuid, userGuid, true, true).WriteInserts(Console.Out);
-        }
+        public static void Main(string[] args) => CommandLineApplication.Execute<Program>(args);
     }
 }
